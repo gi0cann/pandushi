3e6884a42cb0d480a7a1b4a8f80b6135f3616f06
Gionne Cannister <gionnec@gmail.com>
2020-10-31 06:41:03 -0400

    Adds CountInjectionPoints function for counting total injections for the whole request, header, path, query, cookies, and body. Updates HTTPRequest type to add member to take the result of CountInjectionPoints. Adds tests for CountInjectionPoints and NewHTTPRequestFromBytes.

--- fuzzer/fuzzer.go
+++ fuzzer/fuzzer.go
@@ -4,18 +4,38 @@ import (
 	"bufio"
 	"bytes"
 	"compress/gzip"
+	"context"
+	"encoding/json"
+	"fmt"
 	"io"
 	"io/ioutil"
+	"log"
 	"net/http"
 	"net/url"
+	"regexp"
 	"strconv"
 	"strings"
+	"sync"
+	"time"
+
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
+
+// MarkerRegex is the default request injection marker regex string
+const MarkerRegex = `§(.*?)§`
 
 // HTTPRequest represents a fuzzer HTTP request
 type HTTPRequest struct {
-	Request     *http.Request
-	RequestText string // String representation of the Request
+	Request                    *http.Request
+	RequestText                string // String representation of the Request
+	TotalInjectionPoints       int8   // Total number of injection points
+	TotalPathInjectionPoints   int8   // Total number of URL path injection points
+	TotalCookieInjectionPoints int8   // Total number of Cookie injection points
+	TotalHeaderInjectionPoints int8   // Total number of Cookie injection points
+	TotalQueryInjectionPoints  int8   // Total number of Query injection points
+	TotalBodyInjectionPoints   int8   // Total number of Body injection points
 }
 
 // NewHTTPRequestFromBytes take a []byte and returns a HTTPRequest
@@ -34,13 +54,114 @@ func NewHTTPRequestFromBytes(reqstr []byte) (req HTTPRequest, err error) {
 	}
 	request.URL = newurl
 	req.Request = request
+	req.CountInjectionPoints()
 	return req, nil
+}
+
+//CountJSONBody take http.Request and return total amount of parameters
+func CountJSONBody(r *http.Request) (int8, error) {
+	count := int8(0)
+	var e interface{}
+	body, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		return 0, err
+	}
+	err = json.Unmarshal(body, &e)
+	if err != nil {
+		return 0, err
+	}
+	m := e.(map[string]interface{})
+	for k, v := range m {
+		switch vv := v.(type) {
+		case string:
+			//fmt.Printf("String: %s:%s\n", k, v)
+			count++
+		case int:
+			//fmt.Printf("Int: %s:%d\n", k, v)
+			count++
+		case float64:
+			//fmt.Printf("Float: %s:%f\n", k, v)
+			count++
+		case []interface{}:
+			for range vv {
+				count++
+			}
+		case bool:
+			//fmt.Printf("Bool: %s:%t\n", k, v)
+			count++
+		default:
+			fmt.Println(k, "is an unknown type")
+		}
+	}
+
+	//fmt.Println(body)
+	//fmt.Println(len(body))
+	return count, nil
+}
+
+// CountInjectionPoints takes a http.Request and return to total amount of injection points
+func (req *HTTPRequest) CountInjectionPoints() {
+	r := req.Request
+	req.TotalInjectionPoints = int8(0)
+	req.TotalBodyInjectionPoints = int8(0)
+	req.TotalQueryInjectionPoints = int8(len(r.URL.Query()))
+	req.TotalInjectionPoints += req.TotalQueryInjectionPoints
+	req.TotalHeaderInjectionPoints = int8(len(r.Header))
+	req.TotalInjectionPoints += req.TotalHeaderInjectionPoints
+	req.TotalPathInjectionPoints = int8(0)
+	for _, p := range strings.Split(r.URL.Path, "/") {
+		p = strings.TrimSpace(p)
+		if p != "" {
+			req.TotalPathInjectionPoints++
+		}
+	}
+	if req.TotalPathInjectionPoints == 0 {
+		req.TotalInjectionPoints++
+		req.TotalPathInjectionPoints++
+	} else {
+		req.TotalInjectionPoints += req.TotalPathInjectionPoints
+	}
+	req.TotalCookieInjectionPoints = int8(len(r.Cookies()))
+	req.TotalInjectionPoints += req.TotalCookieInjectionPoints
+	ContentType := r.Header.Get("content-type")
+	if ContentType == "application/x-www-form-urlencoded" {
+		err := r.ParseForm()
+		if err == nil {
+			req.TotalBodyInjectionPoints = int8(len(r.PostForm))
+			req.TotalInjectionPoints += req.TotalBodyInjectionPoints
+		}
+	} else if strings.Contains(ContentType, "multipart/form-data") {
+		err := r.ParseMultipartForm(4096000)
+		if err == nil {
+			req.TotalBodyInjectionPoints += int8(len(r.MultipartForm.File))
+			req.TotalBodyInjectionPoints += int8(len(r.MultipartForm.Value))
+			req.TotalInjectionPoints += req.TotalBodyInjectionPoints
+		}
+	} else if strings.Contains(ContentType, "application/json") {
+		jsoncount, err := CountJSONBody(r)
+		if err != nil {
+			fmt.Println(err)
+		} else {
+			req.TotalBodyInjectionPoints += jsoncount
+			req.TotalInjectionPoints += jsoncount
+		}
+	} else if strings.Contains(ContentType, "text/plain") {
+		jsoncount, err := CountJSONBody(r)
+		if err != nil {
+			fmt.Println(err)
+		} else {
+			req.TotalBodyInjectionPoints += jsoncount
+			req.TotalInjectionPoints += jsoncount
+		}
+	}
+
 }
 
 // NewHTTPRequestFromRequest takes a http.Request and returns a HTTPRequest
 func NewHTTPRequestFromRequest(r *http.Request) (req HTTPRequest) {
 	req.Request = r
 	req.RequestText = string("test")
+	req.CountInjectionPoints()
 	return req
 }
 
@@ -62,6 +183,19 @@ func RequestToString(r *http.Request) (string, error) {
 	}
 	RequestStr.WriteString("\r\n" + string(body) + "\r\n")
 	return RequestStr.String(), nil
+}
+
+// IsMarked check for injection markers inside of a request and return true if found or false if not found.
+func (req *HTTPRequest) IsMarked() bool {
+	Marker := regexp.MustCompile(MarkerRegex)
+	return Marker.MatchString(req.RequestText)
+	/*
+		MarkerPositions := Marker.FindAllIndex([]byte(req.RequestText), 0)
+		log.Println(len(MarkerPositions))
+		for _, pos := range MarkerPositions {
+			log.Println(pos)
+		}
+	*/
 }
 
 // HTTPResponse represents a fuzzer HTTP response
@@ -120,4 +254,86 @@ func NewHTTPResponse(baseres *http.Response) (res HTTPResponse, err error) {
 	}
 	res.ResponseText = resStr
 	return res, nil
+}
+
+// Task represents a Fuzzer task
+type Task struct {
+	InjectionTypes []string
+	BaseRequest    HTTPRequest
+	Start          time.Time
+	End            time.Time
+	State          string
+}
+
+// NewTask takes a list of InjectionTypes and HTTPRequest and returns a FuzzerTask
+func NewTask(InjectionTypes []string, BaseRequest HTTPRequest) Task {
+	task := Task{
+		InjectionTypes: InjectionTypes,
+		BaseRequest:    BaseRequest,
+	}
+
+	return task
+}
+
+// Run starts and run a fuzzer Task
+func (f Task) Run() {
+	f.Start = time.Now()
+	var mutex = &sync.Mutex{}
+	var wg sync.WaitGroup
+	for i := 0; i < 10; i++ {
+		wg.Add(1)
+		mutex.Lock()
+		go func() {
+			defer wg.Done()
+			mclient, err := mongo.NewClient(options.Client().ApplyURI("mongodb://localhost:27017"))
+			if err != nil {
+				panic(err)
+			}
+			ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+			err = mclient.Connect(ctx)
+			if err != nil {
+				panic(err)
+			}
+			defer mclient.Disconnect(ctx)
+			defer cancel()
+
+			pandushiDB := mclient.Database("pandushi")
+			taskCollection := pandushiDB.Collection("tasks")
+
+			httpclient := http.Client{
+				Timeout: time.Duration(5 * time.Second),
+			}
+			reqstr, err := RequestToString(f.BaseRequest.Request)
+			if err != nil {
+				fmt.Println(err)
+			}
+
+			fmt.Printf("REQSTR:\n\n%s\n", reqstr)
+
+			resp, err := httpclient.Do(f.BaseRequest.Request)
+			if err != nil {
+				fmt.Println(err)
+			} else {
+				httpres, err := NewHTTPResponse(resp)
+				if err != nil {
+					fmt.Println(err)
+				} else {
+					//fmt.Printf("REQSTR:\n\n%s\n", reqstr)
+					//fmt.Println(httpres.ResponseText)
+					taskResult, err := taskCollection.InsertOne(ctx, bson.D{
+						{Key: "Request", Value: reqstr},
+						{Key: "Response", Value: httpres.ResponseText},
+					})
+					if err != nil {
+						log.Println(err)
+					} else {
+						log.Println(taskResult.InsertedID)
+					}
+				}
+			}
+		}()
+		mutex.Unlock()
+	}
+	wg.Wait()
+	f.End = time.Now()
 }
---
+++ fuzzer/fuzzer_test.go
@@ -0,0 +1,264 @@
+package fuzzer
+
+import (
+	"net/http"
+	"testing"
+)
+
+func TestCountingInjectionPoints(t *testing.T) {
+	tests := []struct {
+		input               string
+		expectedPathCount   int8
+		expectedCookieCount int8
+		expectedHeaderCount int8
+		expectedQueryCount  int8
+		expectedBodyCount   int8
+		expectedTotalCount  int8
+	}{
+		{
+			`GET /?q=hello HTTP/1.1
+Host: github.com
+Connection: close
+Upgrade-Insecure-Requests: 1
+User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.125 Safari/537.36
+Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9
+Sec-Fetch-Site: none
+Sec-Fetch-Mode: navigate
+Sec-Fetch-User: ?1
+Sec-Fetch-Dest: document
+Accept-Encoding: gzip, deflate
+Accept-Language: en-US,en;q=0.9
+
+`,
+			1,
+			0,
+			10,
+			1,
+			0,
+			12,
+		},
+		{
+			`POST /1.1/jot/client_event.json HTTP/1.1
+Host: api.twitter.com
+Connection: close
+Content-Length: 1044
+x-twitter-client-language: en
+x-csrf-token: 29b13902476d063119eb45e64b499f02
+authorization: Bearer AAAAAAAAAAAAAAAAAAAAANRILgAAAAAAnNwIzUejRCOuH5E6I8xnZz4puTs%3D1Zv7ttfk8LF81IUq16cHjhLTvJu4FA33AGWWjCpTnA
+content-type: application/x-www-form-urlencoded
+Accept: */*
+User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.125 Safari/537.36
+x-guest-token: 1298697166744096776
+x-twitter-active-user: no
+Origin: https://twitter.com
+Sec-Fetch-Site: same-site
+Sec-Fetch-Mode: cors
+Sec-Fetch-Dest: empty
+Referer: https://twitter.com/
+Accept-Encoding: gzip, deflate
+Accept-Language: en-US,en;q=0.9
+Cookie: personalization_id="v1_m1X67HiXxMWOO2SVGGlymA=="; guest_id=v1%3A159846850781121896; gt=1298697166744096776; ct0=29b13902476d063119eb45e64b499f02; _twitter_sess=BAh7CSIKZmxhc2hJQzonQWN0aW9uQ29udHJvbGxlcjo6Rmxhc2g6OkZsYXNo%250ASGFzaHsABjoKQHVzZWR7ADoPY3JlYXRlZF9hdGwrCLfQJSx0AToMY3NyZl9p%250AZCIlNDczYzMyNDQwYmQ2YzkxMDBiOGMxNzVkOTZhMmFkMjM6B2lkIiU1ZWE5%250ANDlhODE3ZTQzMDQ4ZWRlNTYxYzFhYjk1NDRhZQ%253D%253D--1e6afb44360f4ac78c7e6f33833704ce424779c0; external_referer=padhuUp37zjgzgv1mFWxJ12Ozwit7owX|0|8e8t2xd8A2w%3D; _ga=GA1.2.1526578839.1598468512; _gid=GA1.2.1748207117.1598468512
+
+category=perftown&log=%5B%7B%22description%22%3A%22rweb%3Aurt%3Aexplore-web_sidebar%3Afetch_Initial%3Asuccess%22%2C%22product%22%3A%22rweb%22%2C%22duration_ms%22%3A1159%7D%2C%7B%22description%22%3A%22rweb%3Aurt%3Aexplore-web_sidebar%3Afetch_Initial%3Aformat%3Asuccess%22%2C%22product%22%3A%22rweb%22%2C%22duration_ms%22%3A1161%7D%2C%7B%22description%22%3A%22rweb%3Ascroller%3Attfv%3Ascroller_v3%22%2C%22product%22%3A%22rweb%22%2C%22duration_ms%22%3A542%7D%2C%7B%22description%22%3A%22rweb%3Aurt%3Aexplore-web_sidebar%3Afetch_Initial%3Asuccess%22%2C%22product%22%3A%22rweb%22%2C%22duration_ms%22%3A887%7D%2C%7B%22description%22%3A%22rweb%3Aurt%3Aexplore-web_sidebar%3Afetch_Initial%3Aformat%3Asuccess%22%2C%22product%22%3A%22rweb%22%2C%22duration_ms%22%3A888%7D%2C%7B%22description%22%3A%22rweb%3Aurt%3Asearch%3Afetch_Initial%3Asuccess%22%2C%22product%22%3A%22rweb%22%2C%22duration_ms%22%3A1342%7D%2C%7B%22description%22%3A%22rweb%3Aurt%3Asearch%3Afetch_Initial%3Aformat%3Asuccess%22%2C%22product%22%3A%22rweb%22%2C%22duration_ms%22%3A1348%7D%5D`,
+			3,
+			8,
+			18,
+			0,
+			2,
+			31,
+		},
+		{
+			`POST / HTTP/1.1
+Host: localhost:8000
+User-Agent: Mozilla/5.0 (X11; Ubuntu; Linux i686; rv:29.0) Gecko/20100101 Firefox/29.0
+Accept: text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8
+Accept-Language: en-US,en;q=0.5
+Accept-Encoding: gzip, deflate
+Cookie: __atuvc=34%7C7; permanent=0; _gitlab_session=226ad8a0be43681acf38c2fab9497240; __profilin=p%3Dt; request_method=GET
+Connection: keep-alive
+Content-Type: multipart/form-data; boundary=---------------------------9051914041544843365972754266
+Content-Length: 554
+
+-----------------------------9051914041544843365972754266
+Content-Disposition: form-data; name="text"
+
+text default
+-----------------------------9051914041544843365972754266
+Content-Disposition: form-data; name="file1"; filename="a.txt"
+Content-Type: text/plain
+
+Content of a.txt.
+
+-----------------------------9051914041544843365972754266
+Content-Disposition: form-data; name="file2"; filename="a.html"
+Content-Type: text/html
+
+<!DOCTYPE html><title>Content of a.html.</title>
+
+-----------------------------9051914041544843365972754266--
+`,
+			1,
+			5,
+			8,
+			0,
+			3,
+			17,
+		},
+		{
+			`POST /api/v2/client/sites/1783312/visit-data?sv=6 HTTP/1.1
+Host: in.hotjar.com
+Connection: close
+Content-Length: 191
+User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.121 Safari/537.36
+Content-Type: text/plain; charset=UTF-8
+Accept: */*
+Origin: https://www.logitech.com
+Sec-Fetch-Site: cross-site
+Sec-Fetch-Mode: cors
+Sec-Fetch-Dest: empty
+Referer: https://www.logitech.com/
+Accept-Encoding: gzip, deflate
+Accept-Language: en-US,en;q=0.9
+
+{"window_width":956,"window_height":1098,"url":"https://www.logitech.com/en-us","r_value":1,"is_vpv":false,"session_only":false,"rec_value":1,"user_id":"c5da6a65-eb05-5eac-9867-36ce7342d1c0"}`,
+			6,
+			0,
+			12,
+			1,
+			8,
+			27,
+		},
+		{
+			`POST /api/v2/client/sites/1783312/visit-data?sv=6 HTTP/1.1
+Host: in.hotjar.com
+Connection: close
+Content-Length: 209
+User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.121 Safari/537.36
+Content-Type: application/json; charset=UTF-8
+Accept: */*
+Origin: https://www.logitech.com
+Sec-Fetch-Site: cross-site
+Sec-Fetch-Mode: cors
+Sec-Fetch-Dest: empty
+Referer: https://www.logitech.com/
+Accept-Encoding: gzip, deflate
+Accept-Language: en-US,en;q=0.9
+
+{"hello":[34.4,34],"window_width":956,"window_height":1098,"url":"https://www.logitech.com/en-us","r_value":1,"is_vpv":false,"session_only":false,"rec_value":1,"user_id":"c5da6a65-eb05-5eac-9867-36ce7342d1c0"}`,
+			6,
+			0,
+			12,
+			1,
+			10,
+			29,
+		},
+	}
+
+	for _, tt := range tests {
+
+		request, err := NewHTTPRequestFromBytes([]byte(tt.input))
+		if err != nil {
+			t.Fatalf("Error creating HTTPRequest from bytes %s\n", err)
+		}
+
+		request.CountInjectionPoints()
+
+		if request.TotalPathInjectionPoints != tt.expectedPathCount {
+			t.Errorf("Total path injection points don't match the expected total. Expected: %d got: %d\n", tt.expectedPathCount, request.TotalPathInjectionPoints)
+		}
+
+		if request.TotalCookieInjectionPoints != tt.expectedCookieCount {
+			t.Errorf("Total cookie injection points don't match the expected total. Expected: %d got: %d\n", tt.expectedCookieCount, request.TotalCookieInjectionPoints)
+		}
+
+		if request.TotalHeaderInjectionPoints != tt.expectedHeaderCount {
+			t.Errorf("Total header injection points don't match the expected total. Expected: %d got: %d\n", tt.expectedHeaderCount, request.TotalHeaderInjectionPoints)
+		}
+
+		if request.TotalQueryInjectionPoints != tt.expectedQueryCount {
+			t.Errorf("Total query injection points don't match the expected total. Expected: %d got: %d\n", tt.expectedQueryCount, request.TotalQueryInjectionPoints)
+		}
+
+		if request.TotalBodyInjectionPoints != tt.expectedBodyCount {
+			t.Errorf("Total body injection points don't match the expected total. Expected: %d got: %d\n", tt.expectedBodyCount, request.TotalBodyInjectionPoints)
+		}
+
+		if request.TotalInjectionPoints != tt.expectedTotalCount {
+			t.Errorf("Total injection points don't match the expected total. Expected: %d got: %d\n", tt.expectedTotalCount, request.TotalInjectionPoints)
+		}
+	}
+}
+
+func TestParsingHTTPRequestFromBytes(t *testing.T) {
+	tests := []struct {
+		input            string
+		expectedMethod   string
+		expectedHost     string
+		expectedProtocol string
+		expectedHeaders  http.Header
+	}{
+		{
+			`GET / HTTP/1.1
+Host: github.com
+Connection: close
+Upgrade-Insecure-Requests: 1
+User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.125 Safari/537.36
+Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9
+Sec-Fetch-Site: none
+Sec-Fetch-Mode: navigate
+Sec-Fetch-User: ?1
+Sec-Fetch-Dest: document
+Accept-Encoding: gzip, deflate
+Accept-Language: en-US,en;q=0.9
+
+`,
+			"GET",
+			"github.com",
+			"HTTP/1.1",
+			map[string][]string{
+				"Connection":                {"close"},
+				"Upgrade-Insecure-Requests": {"1"},
+				"User-Agent":                {"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.125 Safari/537.36"},
+				"Accept":                    {"text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9"},
+				"Sec-Fetch-Site":            {"none"},
+				"Sec-Fetch-Mode":            {"navigate"},
+				"Sec-Fetch-User":            {"?1"},
+				"Sec-Fetch-Dest":            {"document"},
+				"Accept-Encoding":           {"gzip, deflate"},
+				"Accept-Language":           {"en-US,en;q=0.9"},
+			},
+		},
+	}
+
+	for _, tt := range tests {
+
+		request, err := NewHTTPRequestFromBytes([]byte(tt.input))
+		if err != nil {
+			t.Fatalf("Error creating HTTPRequest from bytes %s\n", err)
+		}
+
+		if request.RequestText != tt.input {
+			t.Errorf("request.RequestText doesn't match input. expected:\n%s \n--\n got:\n%s\n", tt.input, request.RequestText)
+		}
+
+		if request.Request.Method != tt.expectedMethod {
+			t.Errorf("request.Request.Method doesn't match input. expected: %s got: %s\n", tt.expectedMethod, request.Request.Method)
+		}
+
+		if request.Request.Host != tt.expectedHost {
+			t.Errorf("request.Request.Host doesn't match input. expected: %s got: %s\n", tt.expectedHost, request.Request.Host)
+		}
+
+		if request.Request.Proto != tt.expectedProtocol {
+			t.Errorf("request.Request.Proto doesn't match input. expected: %s got: %s\n", tt.expectedProtocol, request.Request.Proto)
+		}
+
+		for k := range tt.expectedHeaders {
+			if request.Request.Header.Get(k) != tt.expectedHeaders.Get(k) {
+				t.Errorf("request.Request.Header doesn't match input. expected: %s got: %s\n", tt.expectedHeaders.Get(k), request.Request.Header.Get(k))
+			}
+		}
+
+	}
+
+}
