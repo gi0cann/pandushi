84194a856ee9101b686bbbde016f1e9775324ec6
Gionne Cannister <gionnec@gmail.com>
2020-11-14 11:46:02 -0500

    Adds New function for creating a single payloads.Payload type. Adds CreatePayloadsFromInputTypes function for create multiple payloads.Payloads.

--- payloads/payloads.go
+++ payloads/payloads.go
@@ -1,7 +1,62 @@
 package payloads
 
+import (
+	"context"
+	"strings"
+	"time"
+
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
 // Payload represents a fuzzer input
 type Payload struct {
 	InputType string `json:"type"`
 	Value     string `json:"value"`
+}
+
+// New take payload type and value returns a Payload
+func New(inputtype string, value string) Payload {
+	return Payload{
+		InputType: inputtype,
+		Value:     value,
+	}
+}
+
+// CreatePayloadsFromInputTypes takes and array of Payload InputTypes and an mongodb uri and returns an array of Payloads of that type from mongodb
+func CreatePayloadsFromInputTypes(InputTypes []string, mongodbURI string) ([]Payload, error) {
+	var payloads []Payload
+	var temppayloads []Payload
+	client, err := mongo.NewClient(options.Client().ApplyURI(mongodbURI))
+	if err != nil {
+		return payloads, err
+	}
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	err = client.Connect(ctx)
+	if err != nil {
+		panic(err)
+	}
+	defer client.Disconnect(ctx)
+	defer cancel()
+
+	for _, inputtype := range InputTypes {
+
+		filter := bson.M{"type": strings.ToUpper(inputtype)}
+		pandushiDB := client.Database("pandushi")
+		injectionsCollection := pandushiDB.Collection("injections")
+
+		cursor, err := injectionsCollection.Find(ctx, filter)
+		if err != nil {
+			continue
+		}
+
+		if err = cursor.All(ctx, &temppayloads); err != nil {
+			continue
+		} else {
+			payloads = append(payloads, temppayloads...)
+		}
+
+	}
+	return payloads, nil
 }
